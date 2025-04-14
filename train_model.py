9b4ee1177c48ba1dd9fb5e2de869705671678416
Yograj Mahawar <89453936+yrajm1997@users.noreply.github.com>
2025-04-14 05:21:24 +0000

    files updated

--- train_model.py
+++ train_model.py
@@ -6,12 +6,23 @@ import os
 
 
 # Initialize and train the Random Forest model
-model = RandomForestClassifier(random_state=42)
-model.fit(X_train, y_train)
+n_estimators = 100
+max_depth = 9
+max_features = 7
+random_state = 42
+
+rf_model = RandomForestClassifier(
+    n_estimators = n_estimators, 
+    max_depth = max_depth,
+    max_features = max_features,
+    random_state=42)
+
+rf_model.fit(X_train, y_train)
 print(" Model trained successfully!")
 
+
 # Predict and evaluate
-y_pred = model.predict(X_test)
+y_pred = rf_model.predict(X_test)
 
 def evaluate_model(y_true, y_pred):
     acc = accuracy_score(y_true, y_pred)
@@ -27,8 +38,30 @@ def evaluate_model(y_true, y_pred):
 
 evaluate_model(y_test, y_pred)
 
+# Training and testing accuracy
+training_acc = accuracy_score(rf_model.predict(X_train), y_train)
+testing_acc = accuracy_score(rf_model.predict(X_test), y_test)
+
 # Save trained model
 os.makedirs("trained_model", exist_ok=True)
-joblib.dump(model, "trained_model/rf_model_term_deposit.pkl")
+joblib.dump(rf_model, "trained_model/rf_model_term_deposit.pkl")
 print("\n Model saved at: trained_model/rf_model_term_deposit.pkl")
 
+
+################# MLflow related code below #########################
+
+import mlflow
+
+# Log parameters
+mlflow.log_param("n_estimators", n_estimators)
+mlflow.log_param("max_depth", max_depth)
+mlflow.log_param("max_features", max_features)
+mlflow.log_param("random_state", random_state)
+
+# Log metrics
+mlflow.log_metric("training_accuracy", training_acc)
+mlflow.log_metric("testing_accuracy", testing_acc)
+
+# Log model  
+# Ignore the WARNING saying `Model logged without a signature and input example.`
+mlflow.sklearn.log_model(rf_model, "term-deposit-pred-model")
